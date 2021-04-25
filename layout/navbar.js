674cb4e936c46ee930d2a5445b9592b9729fe562
Parthipan Sekar <parthipan.sekar@xe.com>
2021-04-25 00:59:09 -0400

    add logo

--- layout/navbar.js
+++ layout/navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import LogoSVG from '../assets/logo';
 
+/**
+ *  Navbar Layout
+ * @returns 
+ */
 const Navbar = () => (
-  <div style={{display:"flex", justifyContent:"center", alignItems:"center", background:"lightgray"}}>
-		<p>LOBLAWS TEST</p>
-	</div>
+  <NavbarContainer>
+		<LogoSVG height="150px" width="150px"/>
+		<h1 style={{paddingLeft: "15px"}}>UI ASSESSMENT</h1>
+	</NavbarContainer>
 )
 
 export default Navbar;
+
+const NavbarContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: #eceaea;
+`;
